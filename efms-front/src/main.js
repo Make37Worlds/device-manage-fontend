c5017dc85b89970d410b7fb80035eabfe36ca024
Make37Worlds <77443791+Make37Worlds@users.noreply.github.com>
2023-11-05 16:30:52 +0800

    Update main.js

--- efms-front/src/main.js
+++ efms-front/src/main.js
@@ -143,7 +143,7 @@ Vue.prototype.$notify = Notification;
 Vue.prototype.$message = Message;
 Vue.prototype.$cookie = cookie;
 
-axios.defaults.baseURL = 'http://13.212.26.70:80';
+axios.defaults.baseURL = 'http://54.169.219.171:80';
 
 new Vue({
     el: '#app',
